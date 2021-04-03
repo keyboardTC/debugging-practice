6ba8c2c74c59003808d88af28f5896625498bebd
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2021-04-03 11:56:55 -0400

    Have correct the matching cards logics

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -6,37 +6,49 @@ window.addEventListener('DOMContentLoaded', function() {
 
   cards.forEach(function(card) {
     card.addEventListener('click', function() {
+
+      debugger;
       // If the card has already been matched, ignore it.
       if (card.classList.contains('is-matched')) {
         return;
       }
 
+      debugger;
       // If we haven't selected 2 cards yet, add the current card to the
       // collection of selected cards and apply the correct CSS class.
       if (selectedCards.length < 2) {
         card.classList.add('is-selected');
+        selectedCards.push(card);
       }
-
+      alert(selectedCards);
+      alert(selectedCards.length);
+      debugger;
       // If we have selected two cards, see if they match.
       if (selectedCards.length === 2) {
         var card1 = selectedCards[0];
-        var card2 = selectedCards[0];
+        var card2 = selectedCards[1];
 
+        debugger;
         // If the cards match, add them to the collection of matched cards and
         // apply the correct CSS class.
         if (card1.innerText === card2.innerText) {
           matchedCards.push(card1, card2);
           card1.classList.add('is-matched');
           card2.classList.add('is-matched');
+          selectedCards = [];
+        }else{
+          card1.classList.remove('is-selected');
+          card2.classList.remove('is-selected');
+          selectedCards = [];
         }
 
+        debugger;
         // Regardless of whether or not the cards match, deselect them and reset
         // the collection of matched cards.
-        card1.classList.remove('is-selected');
-        card3.classList.remove('is-selected');
-        selectedCards = [];
+
       }
 
+      debugger;
       // If we've matched all the cards, display a message.
       if (matchedCards.length > cards.length) {
         alert('You matched all the cards, nice job!');
