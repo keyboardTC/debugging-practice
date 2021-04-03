3720031d1dd3d99bf4f15e1e2a95ceca8a0ddc51
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2021-04-03 17:31:29 -0400

    Added js code for Restarte Button.

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -7,28 +7,23 @@ window.addEventListener('DOMContentLoaded', function() {
 
   cards.forEach(function(card) {
     card.addEventListener('click', function() { 
-      // debugger;
       // If the card has already been matched, ignore it.
       if (card.classList.contains('is-matched')) {
         return;
       }
 
-      // debugger;
       // If we haven't selected 2 cards yet, add the current card to the
       // collection of selected cards and apply the correct CSS class.
       if (selectedCards.length < 2) {
         card.classList.add('is-selected');
         selectedCards.push(card);
       }
-      // alert(selectedCards);
-      // alert(selectedCards.length);
-      // debugger;
+
       // If we have selected two cards, see if they match.
       if (selectedCards.length === 2) {
         var card1 = selectedCards[0];
         var card2 = selectedCards[1];
 
-        // debugger;
         // If the cards match, add them to the collection of matched cards and
         // apply the correct CSS class.
         if (card1.innerText === card2.innerText) {
@@ -37,25 +32,38 @@ window.addEventListener('DOMContentLoaded', function() {
           card2.classList.add('is-matched');
           selectedCards = [];
         }else{
+        // Regardless of whether or not the cards match, deselect them and reset
+        // the collection of matched cards. 
           card1.classList.remove('is-selected');
           card2.classList.remove('is-selected');
           selectedCards = [];
         }
-
-        // debugger;
-        // Regardless of whether or not the cards match, deselect them and reset
-        // the collection of matched cards.
-
       }
 
-      // debugger;
       // If we've matched all the cards, display a message.
       if (matchedCards.length === cards.length) {
         alert('You matched all the cards, nice job!');
+
+
+        // This part is used to display the Restart Button if user has completed game
+        // atleaste once      
         count++;
+        var btn = document.getElementById("re-btn");
+        if (count >  0) {
+          btn.style.display = "block";
+          btn.onclick = function(event) {
+            for (index = 0; index < cards.length; index++) {
+              var myCard = cards[index];
+              myCard.classList.remove('is-selected');
+              myCard.classList.remove('is-matched');
+            }
+            matchedCards = [];
+            btn.style.display = "none";
+          }
+        } else {
+          btn.style.display = "none";
+        }
       }
-
-
     });
 
   // Part 2: Allow the user to customize the colour of the cards.
@@ -67,15 +75,9 @@ window.addEventListener('DOMContentLoaded', function() {
     var deckColor = window.location.hash.slice(1)
   
     if (cards && deckColor) {
-      // debugger;
       var className = 'cards--' + deckColor;
-      var newClass = `${className}`;
-      // alert(className);
       card.classList.add(className);
     }
   });
-
-
-
 
 });
